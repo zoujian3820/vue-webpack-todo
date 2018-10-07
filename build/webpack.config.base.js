e889ad8fa025fef383f983b34e6cbc2e67f41d48
MrZou <382066178@qq.com>
2018-10-07 18:03:27 +0800

    commit

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -13,7 +13,7 @@ const config = {
     entry: path.join(__dirname, '../src/index.js'),
     output: {
         filename: 'bundle.[hash:8].js',
-        path: path.join(__dirname, 'dist')
+        path: path.join(__dirname, '../dist')
     },
     module: {
         rules: [
